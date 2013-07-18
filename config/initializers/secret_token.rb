7c803e0f1bcd85405bf53259cf41b19d95890083
D Tucker <danielle@tuckerlabs.com>
2013-07-18 13:17:17 -0700

    Generate the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowTo::Application.config.secret_token = 'd472a665a10ad51eeace3978272c87a4c643369f077dffa9c0f4aa9af6e79e1ecc4c6338458256f4430963ea4830a2685d70ea7b8f2c3de3bf21bbe1c2b573a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HowTo::Application.config.session_store :cookie_store, key: '_how_to_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HowTo::Application.config.session_store :active_record_store
