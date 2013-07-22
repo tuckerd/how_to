927f7e09c1bd2b604d8620a06084c6b48edd5e49
D Tucker <danielle@tuckerlabs.com>
2013-07-22 00:13:02 -0700

    Working on nested resource steps. Having Errors

--- spec/integration/creating_step_spec.rb
+++ spec/integration/creating_step_spec.rb
@@ -2,37 +2,25 @@ require "spec_helper"
 
 feature "Creating Tickets" do
   before do
-    project = Factory(:project, name: "Internet Explorer")
-    user = Factory(:confirmed_user, email: "ticketee@example.com")
+    project = Factory(:topic, title: "Cutting a Pizza")
 
     visit '/'
-    click_link "Internet Explorer"
-    click_link "New Ticket"
-    within("h2")  { page.should have_content("New Ticket") }
+    click_link "Cutting a Pizza"
+    click_link "New Step"
+    within("h1")  { page.should have_content("New Step") }
   end
 
-  scenario "Creating a ticket" do
-    fill_in "Title", with: "Non-standards compliance"
-    fill_in "Description", with: "My pages are ugly!"
-    click_button "Create Ticket"
-    page.should have_content("Ticket has been created.")
-    within('#ticket #author')do
-      page.should have_content("Created by ticketee@example.com")
-    end
+  scenario "Creating a step" do
+    fill_in "Step Number", with: "1"
+    fill_in "Description", with: "Cut your pizza's into 12!"
+    click_button "Create Step"
+    page.should have_content("Step has been created.")
   end
 
-  scenario "Creating a ticket without valid attributes fails" do
-    click_button "Create Ticket"
-    page.should have_content("Ticket has not been created.")
-    page.should have_content("Title can't be blank")
-    page.should have_content("Description can't be blank")
-  end
-
-  scenario "Description must be longer than 10 characters" do
-    fill_in "Title", with: "Non-standards compliance"
-    fill_in "Description", with: "it sucks"
-    click_button "Create Ticket"
-    page.should have_content("Ticket has not been created.")
-    page.should have_content("Description is too short")
+  scenario "Creating a step without valid attributes fails" do
+    click_button "Create Step"
+    page.should have_content("Step has not been created.")
+    page.should have_content("Order can't be blank")
+    page.should have_content("Content can't be blank")
   end
 end
